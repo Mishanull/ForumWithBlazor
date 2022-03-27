eb1cec2e0a76c1e732677e0b8478d4e376c9371c
Mike Abbey <mihaiavram72@gmail.com>
2022-03-27 12:21:41 +0200

    subforum left for later

--- Entities/Forum.cs
+++ Entities/Forum.cs
@@ -3,10 +3,9 @@
 public class Forum
 {
     public ICollection<Post>? Posts { get; set; }
-    public ICollection<SubForum>? SubForums { get; set; }
+   
     public Forum()
     {
-        SubForums = new List<SubForum>();
         Posts = new List<Post>();
     }
 }
